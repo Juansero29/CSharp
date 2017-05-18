eb9c1d76a50d47ba6098b40c4a60815f24a365e2
Juansero29 <juansero29@gmail.com>
2017-05-18 20:56:57 +0200

    - Added a simple Metro project

---
+++ MiscTests/TestsWPF/MetroSimpleApp/MetroSimpleApp.Models/Image.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroSimpleApp.Models
+{
+    public class Image
+    {
+        public string Nom { get; set; }
+    }
+}
